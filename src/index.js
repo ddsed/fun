b95a0bfbf6aff983fdb6a75fa5f6d47892f55e28
Dasha Sedova <dashassed@gmail.com>
2024-02-23 19:16:08 +0100

    fix: deploy 2.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from "react-router-dom";
 import './index.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <HashRouter>
   <React.StrictMode>
-    <App />
+  <Routes>
+      <Route path="/" element={ <App />}/>
+  </Routes>
   </React.StrictMode>
+  </HashRouter>
 );
