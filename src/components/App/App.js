8f4c06cd0dd9ed0a65e3739ec52ce8f48b77ed9d
Dasha Sedova <dashassed@gmail.com>
2024-02-23 16:35:44 +0100

    feat: answer yes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactPlayer from 'react-player'
 
 function App() {
+  const[yesClicked, setYesClicked] = useState(false)
+
   return (
     <div className="page">
+      {!yesClicked ?
+      <>
       <img src={require(`../../images/cover.jpg`)} alt="Gosling" className="page__image" />
       <h1 className='page__question'>Wanna switch your {'{'}AS IS{'}'} = Ekaterinburg to {'{'}TO BE{'}'} = Kaliningrad?</h1>
       <div className='page__button-container'>
         <button className='page__button-no button'>Nope :(</button>
-        <button className='page__button-yes button'>Yep</button>
+        <button className='page__button-yes button' onClick={() => { setYesClicked(true)}}>Yep</button>
       </div>
+      </>
+      :
+      <>
+      <div className='page__player-wrapper'>
+          <ReactPlayer
+            className='page__react-player'
+            url='videos/yes.mov'
+            playing={true}
+            loop={true}
+          />
+      </div>
+      <h1 className='page__title-yes'>!!! I am glad AF !!!</h1>
+      </>
+      }
     </div>
   );
 }
